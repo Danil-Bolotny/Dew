91fe86e3983c59436d1bebaa13a027095caf69b5
Danil-Bolotny <d.s.bolotnyi@student.khai.edu>
2021-12-22 22:49:39 +0200

    Chat-bot

---
+++ Chat-bot/src/St1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class St1 {
+    public class SimpleBot
+    {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello! My name is Aid.");
+            System.out.println("I was created in 2018.");
+        }
+    }
+}
