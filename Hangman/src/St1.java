5431b24a0f975440b25f145c8db3eca1b03ceee6
Danil-Bolotny <d.s.bolotnyi@student.khai.edu>
2021-12-22 22:51:51 +0200

    Hangman

---
+++ Hangman/src/St1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class St1 {
+    public static void main(String[] args) {
+        System.out.println("H A N G M A N");
+        System.out.println("The game will be available soon.");
+    }
+}
