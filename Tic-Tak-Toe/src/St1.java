28dfd601d2811de9d6d2a165b779041d3ef23ada
Danil-Bolotny <d.s.bolotnyi@student.khai.edu>
2021-12-22 22:54:10 +0200

    Tic-Tak-Toe

---
+++ Tic-Tak-Toe/src/St1.java
@@ -0,0 +1,8 @@
+package tictactoe;
+public class St1 {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X X");
+        System.out.println("O O X");
+    }
+}
